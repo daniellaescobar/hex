57fe9deae5600acbaff4454a0457bfa51d16d912
Daniella Escobar <descobar0303@gmail.com>
2022-04-25 17:48:02 -0400

    json

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+// Get the elements
+var colorCards = document.querySelectorAll(".color-card");
+colorCards.forEach((colorCard) => {
+  var hex = colorCard.querySelector(".hex");
+  var color = colorCard.querySelector(".color");
 
+  var name = colorCard.querySelector(".name");
+  console.log(hex.innerText);
+  console.log(name.innerText);
+  console.log(color.style.backgroundColor);
+});
+
+var button = document.querySelector("button");
+
+// Define the action
+
+fetch("../colors-list.json");
+
+// Wire up the event with the action
