7cac4c4a1ce2d10b6b46783510fbbb3764ef3be1
daniellaescobar <daniellaescobar@users.noreply.github.com>
2022-04-26 23:46:34 +0000

    deploy: c4270fd672250d084c520ec7748868c1f9ad827f

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const base = window.location.href.includes("github.io")
+  ? `/${window.location.pathname.split("/")[1]}`
+  : "";
+
 // Get the elements
 var colorCards = document.querySelectorAll(".color-card");
 colorCards.forEach((colorCard) => {
@@ -14,6 +18,83 @@ var button = document.querySelector("button");
 
 // Define the action
 
-fetch("../colors-list.json");
+fetch("../colors")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 // Wire up the event with the action
+
+//write a function that gets 6 random numbers that occur within a range
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getFiveRandomNumber(min, max) {
+  var numbers = [];
+  while (numbers.length < 5) {
+    var number = getRandomNumber(min, max);
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+  return numbers;
+}
+
+// var promises = getFiveRandomNumbers(0, 14156).map((n) => {
+//   return fetch(`/colors/${n}`);
+// });
+function getColors() {
+  var promises = getFiveRandomNumber(0, 14156).map((n) => {
+    return fetch(`${base}/colors/${n}.json`);
+  });
+  console.log(promises);
+  //Wait for all responses of the colors
+  Promise.all(promises).then((responses) => {
+    //Wait until all of the colors are parsed
+    Promise.all(responses.map((response) => response.json())).then((data) => {
+      console.log(data);
+      data.forEach((color, index) => {
+        console.log(color);
+        updateColorCard(color, index);
+      });
+    });
+  });
+}
+
+function updateColorCard(color, index) {
+  console.log(color.name);
+  var colorCard = colorCards[index];
+  var hex = colorCard.querySelector(".hex");
+  var backgroundColor = colorCard.querySelector(".color");
+  var name = colorCard.querySelector(".name");
+  name.innerText = color.name;
+  backgroundColor.style.backgroundColor = `rgb(${color.red},${color.green},${color.blue})`;
+  hex.innerText = rgbToHex(color.red, color.green, color.blue);
+}
+
+function componentToHex(hex) {
+  var hexidecimal = hex.toString(16);
+  return hexidecimal.length == 1 ? "0" + hexidecimal : hexidecimal;
+}
+
+function rgbToHex(red, green, blue) {
+  return (
+    "#" + componentToHex(red) + componentToHex(green) + componentToHex(blue)
+  );
+}
+
+button.addEventListener("click", getColors);
+getColors();
+// var numbers = getRandomNumber(0, 14156);
+
+// // max = 14156
+
+// var node = document.getElementById("colorCard");
+
+// function createBoxes(numBoxes){
+//     for (let i = 0; i < numBoxes, i++) {
+//         index = getRandomNumber(0,14156);
+//         name =
+//     }
+// }
